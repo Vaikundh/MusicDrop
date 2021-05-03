42f2f45084d7487d02eb92931309daa7ed0bf9f6
zengchanel <zengchanel@berkeley.edu>
2021-05-03 01:00:20 -0700

    set up user database

--- play-uauth/auth.js
+++ play-uauth/auth.js
@@ -4,7 +4,11 @@ const db = firebase.firestore();
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
-        fixdisplay(user);
+        db.collection('users').doc(user.uid).onSnapshot(doc => {
+            fixdisplay(user, doc.data());
+        }, err => {
+            console.log(error.message)
+        });
     } else {
         fixdisplay();
     }
@@ -21,6 +25,13 @@ signupForm.addEventListener('submit', (e) => {
 
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            photoURL: signupForm['signup-photoURL'].value,
+            locations: 0,
+            added: 0,
+            upvotes: 0
+        });
+    }).then(() => {
         signupForm.reset();
     }).catch(err => {
         alert(err.message);
