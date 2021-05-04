53a56260c18f1c7673680fe7f24abd6683a51de5
Lian Ting <l_ting@berkeley.edu>
2021-05-04 00:08:30 -0700

    lian's app.js

--- playlist-discover/src/App.js
+++ playlist-discover/src/App.js
@@ -1,6 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
 import MapSection from "./Pages/Map/Map.jsx";
+import NavBar from "./Pages/NavBar/nav";
+import Login from "./Pages/LogIn/login";
+import Home from "./Pages/Home/home"
+import Toolbar from '@material-ui/core/Toolbar';
+import Fab from '@material-ui/core/Fab';
 //Add other pages here
 
 const location = {
@@ -12,8 +17,36 @@ const location = {
 
 function App() {
   return (
-    <div className = "App">
-      <MapSection location={location} zoomLevel={18}></MapSection>
+    
+    
+    <div>
+      <div>
+      <Toolbar className = "Toolbar">
+      <div className = "Square">
+        <h1 className="title">Nav</h1>
+      </div>
+      <div className = "Square Buttons">
+        <Fab className="Square" variant='extended'>
+          My Profile
+        </Fab>
+        <Fab variant='extended'>
+          About
+        </Fab>
+        <Fab variant='extended'>
+          Map
+        </Fab>
+        <Fab variant='extended'>
+          Login
+        </Fab>
+      </div>
+    </Toolbar>
+  </div>
+  
+      <div className = "App">
+        <Home></Home>
+        <Login></Login>
+        <MapSection location={location} zoomLevel={18}></MapSection>
+      </div>
     </div>
   );
 }
