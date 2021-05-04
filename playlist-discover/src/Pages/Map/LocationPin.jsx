9547bb06e119205b334fd7ab26397d8634c16a42
Vaikundh <vaikundh.j@gmail.com>
2021-05-04 00:42:19 -0700

    some map updates

--- playlist-discover/src/Pages/Map/LocationPin.jsx
+++ playlist-discover/src/Pages/Map/LocationPin.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Icon } from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/map-marker'
 
-function openGladePlaylist() {
-    if(onclick===true) window.open("http://google.com");
+ const LocationPin = ({ text }) => {
+  const openGladePlaylist = () => {
+     window.open("http://google.com")
 }
-export const LocationPin = ({ text }) => (
-    <div className="pin" onClick={openGladePlaylist()} > 
+
+  return(
+    <div className="pin" onClick={ openGladePlaylist} > 
       <Icon icon={locationIcon} className="pin-icon" />
       <p className="pin-text">{text}</p>
     </div>
-  )
+  )}
 
+export default LocationPin
