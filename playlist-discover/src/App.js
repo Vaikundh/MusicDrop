2b854746edca7faa86adb78dc983cbb656b57899
Vaikundh <vaikundh.j@gmail.com>
2021-05-04 00:47:18 -0700

    final merge conflict?

--- playlist-discover/src/App.js
+++ playlist-discover/src/App.js
@@ -1,6 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import MapSection from "./Pages/Map/Map.jsx";
+import PersonalPage from "./Pages/PersonalPage/PersonalPage.jsx";
+import About from "./Pages/About/About.jsx"
+import Navigation from "./Pages/PageComponents/Navigation.jsx"
+import Footer from "./Pages/PageComponents/Footer.jsx"
+import Home from "./Pages/Home.jsx"
+import LogIn from "./Pages/LogIn/login.jsx"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //Add other pages here
 
 const location = {
@@ -9,12 +16,21 @@ const location = {
   lng: -122.08427,
 }
 
-
 function App() {
   return (
-    <div className = "App">
-      <MapSection location={location} zoomLevel={18}></MapSection>
-    </div>
+      <div className="App">
+        <Router>
+          <Navigation />
+          <Switch>
+            <Route path="/" exact component={() => <Home />} />
+            <Route path="/Login" exact component={() => <LogIn />} />
+            <Route path="/Map" exact component={() => <MapSection location={location} zoomLevel={18}></MapSection>} />
+            <Route path="/PersonalPage" exact component={() => <PersonalPage />} />
+            <Route path="/About" exact component={() => <About />} />
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
   );
 }
 
